fbe478fb93c9381ba5be54233e1fcf82cee8fdc8
tis2023 <fh.tis2023@gmail.com>
2023-05-02 17:51:08 +0900

    added spaces.

--- src/main/java/jp/co/sss/training/entity/Item.java
+++ src/main/java/jp/co/sss/training/entity/Item.java
@@ -17,18 +17,23 @@ public class Item {
     public Integer getId() {
         return id;
     }
+    
     public void setId(Integer id) {
         this.id = id;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public Integer getPrice() {
         return price;
     }
+
     public void setPrice(Integer price) {
         this.price = price;
     }
